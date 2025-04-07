bec2f25f78725300379758ce136f0f31115c363c
ThalesFortes <thales_pro@outlook.com>
2025-04-07 11:33:51 -0300

    Iniciando funcionalidades JS

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const list = [
+  {id:1 , task:"Implementar tela de listagem de tarefas" , tag:"frontend", create:new Date()},
+  {id:2 , task:"Implementar protótipo da listagem de tarefas" , tag:"ux", create:new Date()}
+]
